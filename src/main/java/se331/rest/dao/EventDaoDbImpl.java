ae36e18d9993acf5d11877623c6925747aa64bc5
Nattaparin <nattaparin455@gmail.com>
2022-09-15 22:58:50 +0700

    3.7

--- src/main/java/se331/rest/dao/EventDaoDbImpl.java
+++ src/main/java/se331/rest/dao/EventDaoDbImpl.java
@@ -1,8 +1,8 @@
 package se331.rest.dao;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import se331.rest.entity.Event;
 import se331.rest.repository.EventRepository;
@@ -10,6 +10,7 @@ import se331.rest.repository.EventRepository;
 import java.util.List;
 
 @Repository
+@Profile("db")
 public class EventDaoDbImpl implements EventDao {
     @Autowired
     EventRepository eventRepository ;
@@ -18,7 +19,7 @@ public class EventDaoDbImpl implements EventDao {
         return Math.toIntExact(eventRepository.count());
     }
     @Override
-    public List<Event> getEvents ( Integer pageSize, Integer page ){
+    public List<Event> getEvents (Integer pageSize, Integer page ){
         List<Event> events = eventRepository.findAll();
         pageSize = pageSize == null ? events.size() : pageSize;
         page = page == null ? 1 : page;
